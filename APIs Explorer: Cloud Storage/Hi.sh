e3fd3e4f42f829fd385328a58708f71deded1393
manavyugai <manavyugai@gmail.com>
2026-08-20 10:24:28 +0530

    Update Hi.sh

--- APIs Explorer: Cloud Storage/Hi.sh
+++ APIs Explorer: Cloud Storage/Hi.sh
@@ -54,12 +54,11 @@ echo "${CYAN}${BOLD}━━━━━━━━━━━━━━━━━━━━
 echo "${PURPLE}${BOLD}  [2] DOWNLOAD SAMPLE FILES${RESET}"
 echo "${CYAN}${BOLD}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${RESET}"
 
-curl -# -LO https://raw.githubusercontent.com/GoogleCloudPlatform/cloud-storage-samples/main/sample-files/demo-image1.png
+curl -LO https://raw.githubusercontent.com/manavyugai/Cloud-Monitoring/main/APIs%20Explorer%3A%20Cloud%20Storage/demo-image1.png
 
-curl -# -LO https://raw.githubusercontent.com/GoogleCloudPlatform/cloud-storage-samples/main/sample-files/demo-image2.png
+curl -LO https://raw.githubusercontent.com/manavyugai/Cloud-Monitoring/main/APIs%20Explorer%3A%20Cloud%20Storage/demo-image2.png
 
-curl -# -LO https://raw.githubusercontent.com/GoogleCloudPlatform/cloud-storage-samples/main/sample-files/demo-image1-copy.png
-
+curl -LO https://raw.githubusercontent.com/manavyugai/Cloud-Monitoring/main/APIs%20Explorer%3A%20Cloud%20Storage/demo-image1-copy.png
 echo "${GREEN}${BOLD}✓ Sample files downloaded${RESET}"
 echo
 
